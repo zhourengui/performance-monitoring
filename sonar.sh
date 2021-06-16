d91c1a6f1661e5c3cdc85d75fa6f375a29c0aec4
周壬癸 <729049182@qq.com>
2021-06-16 15:11:30 +0800

    feat: sonar check

---
+++ sonar.sh
@@ -0,0 +1,5 @@
+sonar-scanner \
+  -Dsonar.projectKey=performance-monitoring \
+  -Dsonar.sources=./src \
+  -Dsonar.host.url=http://localhost:9000 \
+  -Dsonar.login=78316f542d73004d010a9decae5bb27490394e2a
